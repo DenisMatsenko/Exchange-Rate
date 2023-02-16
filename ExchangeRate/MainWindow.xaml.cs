279e3baba5d90f525de51c8ab9164f7d824ae0c1
hard.tender.blade <matsenkodenis0@gmail.com>
2023-02-16 21:19:05 +0100

    Box design

--- ExchangeRate/MainWindow.xaml.cs
+++ ExchangeRate/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -20,14 +21,57 @@ namespace ExchangeRate
         public MainWindow()
         {
             InitializeComponent();
+            InitMyCurrencies();
         }
 
         private void ExcRate_Click(object sender, RoutedEventArgs e)
         {
-            Grid MainGrid = new Grid {
+
+        }
+
+        private void InitMyCurrencies()
+        {
+            if (!File.Exists("MyCurrencies.txt"))
+                InitMyCurrenciesFile();
+
+            CreateBoxes();
+
+        }
+
+        private void CreateBoxes()
+        {
+            string[] MyCurrencies = File.ReadAllLines("MyCurrencies.txt");
+
+            for (int i = 0; i < MyCurrencies.Length; i++)
+            {
+                string[] OneCurrencies = MyCurrencies[i].Split('-');
+
+                CreateNewExchangeBox(OneCurrencies[0], OneCurrencies[1]);
+            }
+        }
+
+        private void InitMyCurrenciesFile()
+        {
+            File.Create("MyCurrencies.txt");
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateNewExchangeBox(string LeftCurrence, string RightCurrence)
+        {
+            Grid MainGrid = new Grid
+            {
                 Width = 380,
                 Height = 50,
-                Margin = new Thickness{ 
+                Margin = new Thickness
+                {
                     Bottom = 10,
                     Left = 10,
                     Right = 10,
@@ -36,11 +80,19 @@ namespace ExchangeRate
                 Background = Brushes.White,
             };
 
-            TextBlock ArrowText = new TextBlock {
+            TextBlock ArrowText = new TextBlock
+            {
                 Text = "-",
                 FontSize = 30,
                 VerticalAlignment = VerticalAlignment.Top,
                 HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness
+                {
+                    Bottom = 0,
+                    Left = 0,
+                    Right = 25,
+                    Top = 0,
+                },
             };
 
             TextBlock ValueChangesText = new TextBlock
@@ -49,19 +101,27 @@ namespace ExchangeRate
                 FontSize = 12,
                 VerticalAlignment = VerticalAlignment.Bottom,
                 HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness
+                {
+                    Bottom = 0,
+                    Left = 0,
+                    Right = 25,
+                    Top = 0,
+                },
             };
 
             MainGrid.Children.Add(ValueChangesText);
             MainGrid.Children.Add(ArrowText);
 
-            Grid LeftPartGrid = new Grid {
-                HorizontalAlignment= HorizontalAlignment.Left,
+            Grid LeftPartGrid = new Grid
+            {
+                HorizontalAlignment = HorizontalAlignment.Left,
             };
 
-            TextBlock LeftPartMainTB = new TextBlock {
-                Text = "USD",
+            TextBlock LeftPartMainTB = new TextBlock
+            {
+                Text = LeftCurrence,
                 FontSize = 25,
-                Background = Brushes.Red,
                 VerticalAlignment = VerticalAlignment.Top,
                 HorizontalAlignment = HorizontalAlignment.Left,
             };
@@ -70,7 +130,6 @@ namespace ExchangeRate
             {
                 Text = "uss",
                 FontSize = 15,
-                Background = Brushes.Blue,
                 VerticalAlignment = VerticalAlignment.Bottom,
                 HorizontalAlignment = HorizontalAlignment.Left,
             };
@@ -95,14 +154,20 @@ namespace ExchangeRate
 
             Grid RightPartGrid = new Grid
             {
+                Margin = new Thickness
+                {
+                    Bottom = 0,
+                    Left = 0,
+                    Right = 25,
+                    Top = 0,
+                },
                 HorizontalAlignment = HorizontalAlignment.Right,
             };
 
             TextBlock RightPartMainTB = new TextBlock
             {
-                Text = "CZK",
+                Text = RightCurrence,
                 FontSize = 25,
-                Background = Brushes.Red,
                 VerticalAlignment = VerticalAlignment.Top,
                 HorizontalAlignment = HorizontalAlignment.Right,
             };
@@ -111,7 +176,6 @@ namespace ExchangeRate
             {
                 Text = "czkkkkk",
                 FontSize = 15,
-                Background = Brushes.Blue,
                 VerticalAlignment = VerticalAlignment.Bottom,
                 HorizontalAlignment = HorizontalAlignment.Right,
             };
@@ -134,19 +198,41 @@ namespace ExchangeRate
             RightPartGrid.Children.Add(RightPartDownTB);
             RightPartGrid.Children.Add(RightPartNumTB);
 
+            StackPanel BoxButtonsPanel = new StackPanel {
+                Width = 20,
+                HorizontalAlignment = HorizontalAlignment.Right,
+            };
+
+            Button XButton = new Button {
+                Content = "X",
+                FontSize = 11,
+                BorderThickness = new Thickness(0),
+            };
+
+            Button UpButton = new Button
+            {
+                Content = "▲",
+                FontSize = 11,
+                BorderThickness = new Thickness(0),
+            };
+
+            Button DownButton = new Button
+            {
+                Content = "▼",
+                FontSize = 11,
+                BorderThickness = new Thickness(0),
+            };
+
+            BoxButtonsPanel.Children.Add(XButton);
+            BoxButtonsPanel.Children.Add(UpButton);
+            BoxButtonsPanel.Children.Add(DownButton);
+
+
             MainGrid.Children.Add(RightPartGrid);
             MainGrid.Children.Add(LeftPartGrid);
+            MainGrid.Children.Add(BoxButtonsPanel);
 
             MainStackPanel.Children.Add(MainGrid);
-        }
-
-        private void Settings_Click(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private void About_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
